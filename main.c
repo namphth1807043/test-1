95855bf745b78ad8148449ba3527906c869fe79b
hoainam <namphth1807043@fpt.edu.vn>
2018-09-25 11:11:55 +0700

    btvn

--- main.c
+++ main.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
+#include <string.h>
 
-int countUnsignedNumber(int Array[], int Size) {
-
-    int sum = 0;
-    for (int i = 0; i < Size; i++) {
-        if (Array[i] > 0)
-            sum = sum + Array[i];
-    }
-    return sum;
+void noiHoVaTen(char ho[], char ten[]) {
+    printf("Do dai cua ho la: %d\n", strlen(ho));
+    printf("Do dai cua ten la: %d\n", strlen(ten));
+    int dodaiHo = strlen(ho);
+    int dodaiTen = strlen(ten);
+    int tong = dodaiHo + dodaiTen;
+    printf("Do dai cua ca ho va ten la %d\n", tong);
+    tong++;
+    char hovaten[tong];
+    strcat(hovaten, ho);
+    strcat(hovaten, " ");
+    strcat(hovaten, ten);
+    printf("%s", hovaten);
 }
 
 int main() {
-    int Size;
-    printf("Nhap so luong phan tu: \n");
-    scanf("%d", &Size);
-    int Array[Size];
-    for (int i = 0; i < Size; ++i) {
-        printf("Nhap phan tu tai index %d: ", i);
-        scanf("%d", &Array[i]);
+    char ho[20], ten[20];
+
+    // kiểm tra độ dài họ.
+    while (1 == 1) {
+        printf("Vui long nhap ho cua ban: ");
+        gets(ho);
+        if (strlen(ho) > 20) {
+            printf("Loi !!!!\n");
+            printf("Ban da nhap qua gioi han ky tu. Vui long nhap lai. \n");
+        } else
+            break;
     }
-    int result = countUnsignedNumber(Array, Size);
-    printf("Tong cac so nguyen duong la: %d", result);
+
+
+
+// kiểm tra độ dài tên.
+    while (1 == 1) {
+        printf("Vui long nhap ten cua ban: ");
+        gets(ten);
+        if (strlen(ten) > 20) {
+            printf("Loi !!!!\n");
+            printf("Ban da nhap qua gioi han ky tu. Vui long nhap lai. \n");
+        } else
+            break;
+    }
+    printf("Ho va ten cua ban la: ");
+    noiHoVaTen(ho, ten);
     return 0;
 }
